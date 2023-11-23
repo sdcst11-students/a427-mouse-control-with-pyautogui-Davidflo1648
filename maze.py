d305725e4c21af8eb2378fe7532266caff8ba1f0
Davidflo1648 <144282155+Davidflo1648@users.noreply.github.com>
2023-11-23 13:29:37 -0800

    maze updated

---
+++ maze.py
@@ -0,0 +1,29 @@
+import pyautogui
+
+#location = pyautogui.locateOnScreen('square.webp')
+#print("locateOnScreen:", location)
+
+pyautogui.moveTo(953,206)
+pyautogui.moveTo(1020,206)
+pyautogui.moveTo(1028,310)
+pyautogui.moveTo(993,304)
+pyautogui.moveTo(995,440)
+pyautogui.moveTo(1064,436)
+pyautogui.moveTo(1059,408)
+pyautogui.moveTo(1032,402)
+pyautogui.moveTo(1029,338)
+pyautogui.moveTo(1095,335)
+pyautogui.moveTo(1095,271)
+pyautogui.moveTo(1165,271)
+pyautogui.moveTo(1165,235)
+pyautogui.moveTo(1235,235)
+pyautogui.moveTo(1235,301)
+pyautogui.moveTo(1166,301)
+pyautogui.moveTo(1166,404)
+pyautogui.moveTo(1133,404)
+pyautogui.moveTo(1133,510)
+pyautogui.moveTo(1200,510)
+pyautogui.moveTo(1200,476)
+
+pyautogui.mouseInfo()
+
